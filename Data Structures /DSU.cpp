e205791b30a313d85863ebb47563afb927fa7c56
Ziad-Abdou1 <ziad.abdou1980@gmail.com>
2025-01-24 20:55:20 +0200

    Update DSU.cpp

--- Data Structures /DSU.cpp
+++ Data Structures /DSU.cpp
@@ -1,14 +1,15 @@
 struct DSU {
-    vector<int> par, rnk, sz;
+    vector<int> par, sz;
     int comps;
 
-    DSU(int n) : par(n + 1), rnk(n + 1, 0), sz(n + 1, 1), comps(n) {
+    DSU(int n) : par(n + 1), sz(n + 1, 1), comps(n) {
         iota(begin(par), end(par), 0);
     }
 
     int find(int u) {
         return (par[u] == u ? u : (par[u] = find(par[u])));
     }
+
     bool same(int u, int v) {
         return find(u) == find(v);
     }
@@ -16,6 +17,7 @@ struct DSU {
     int get_size(int u) {
         return sz[find(u)];
     }
+
     bool merge(int u, int v) {
         if ((u = find(u)) == (v = find(v))) return false;
         if (sz[u] > sz[v]) swap(u, v);
